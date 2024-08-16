632d156018c0fe57746e5bd1d9088deca403321d
Lucas Kissmann <lucaskissmann@gmail.com>
2024-08-16 11:25:07 -0300

    fix: add find author by name

--- src/main/java/com/library/api/repositories/AuthorRepository.java
+++ src/main/java/com/library/api/repositories/AuthorRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.library.api.modules.authors.Author;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-	
+
+    Optional<Author> findAuthorByName(String authorName);
 }
