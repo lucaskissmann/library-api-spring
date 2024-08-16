a73bcce12375ce679afdaaa8e3b78a273f97b6ef
Lucas Kissmann <lucaskissmann@gmail.com>
2024-08-16 11:28:21 -0300

    feat: add test cases to AuthorService test

--- src/test/java/com/library/api/authors/service/AuthorServiceTest.java
+++ src/test/java/com/library/api/authors/service/AuthorServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import com.library.api.modules.authors.dtos.UpdateAuthorDTO;
+import com.library.api.modules.authors.validations.AuthorValidator;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -20,10 +22,10 @@ import com.library.api.helpers.exceptions.NotFoundException;
 import com.library.api.modules.authors.Author;
 import com.library.api.modules.authors.dtos.AuthorRequestDTO;
 import com.library.api.modules.authors.dtos.AuthorResponseDTO;
-import com.library.api.modules.authors.mappers.AuthorMapper;
 import com.library.api.repositories.AuthorRepository;
 import com.library.api.services.AuthorService;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -34,6 +36,7 @@ public class AuthorServiceTest {
 	AuthorRequestDTO invalidAuthorRequestDTO = AuthorStub.createInvalidAuthorRequestDTO();
 	AuthorRequestDTO authorRequestStub = AuthorStub.createAuthorRequestDTO();
 	AuthorResponseDTO authorResponseStub = AuthorStub.createAuthorResponseDTO();
+	UpdateAuthorDTO updateAuthorDTO = AuthorStub.updateAuthorDTO();
 
 	private Validator validator;
 
@@ -41,7 +44,7 @@ public class AuthorServiceTest {
 	private AuthorRepository authorRepository;
 
 	@Mock
-	private AuthorMapper authorMapper;
+	private List<AuthorValidator> validators;
 
 	@InjectMocks
 	private AuthorService authorService;
@@ -51,14 +54,16 @@ public class AuthorServiceTest {
 	public void setUp() {
 		MockitoAnnotations.openMocks(this);
 		validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+		AuthorValidator mockValidator = mock(AuthorValidator.class);
+		doNothing().when(mockValidator).validate(any(Author.class));
+		validators.add(mockValidator);
 	}
 
 	@Test
 	@DisplayName("[SERVICE] Deve criar um autor")
 	public void shouldCreateValidAuthor() {
 		//Arrange + Act
-		when(this.authorMapper.toEntity(authorRequestStub)).thenReturn(authorStub);
-		when(this.authorMapper.toResponseDTO(authorStub)).thenReturn(authorResponseStub);
 		AuthorResponseDTO responseDTO = authorService.create(authorRequestStub);
 
 		//Assert
@@ -90,13 +95,58 @@ public class AuthorServiceTest {
 	@DisplayName("[SERVICE] Deve encontrar um autor para o ID informado")
 	public void shouldFindAuthorById() {
 		when(authorRepository.findById(authorStub.getId())).thenReturn(Optional.of(authorStub));
-		when(authorMapper.toResponseDTO(authorStub)).thenReturn(authorResponseStub);
 
 		AuthorResponseDTO foundAuthor = authorService.getAuthor(authorStub.getId());
 
 		assertNotNull(foundAuthor);
 		assertEquals(authorStub.getId(), foundAuthor.getId());
 		assertEquals(authorStub.getName(), foundAuthor.getName());
+	}
+
+	@Test
+	@DisplayName("[SERVICE] Deve atualizar um autor para o ID informado")
+	public void shouldUpdateAuthor() {
+		Long authorId = 1L;
+
+		when(authorRepository.findById(authorStub.getId())).thenReturn(Optional.of(authorStub));
+		when(authorRepository.save(authorStub)).thenReturn(authorStub);
+
+		AuthorResponseDTO result = authorService.update(updateAuthorDTO, authorId);
+
+		assertNotNull(result);
+		assertEquals(result.getName(), updateAuthorDTO.getName());
+		assertEquals(result.getIdade(), updateAuthorDTO.getIdade());
+		assertEquals(result.getId(), authorStub.getId());
+
+		verify(authorRepository).findById(authorId);
+		verify(authorRepository).save(authorStub);
+	}
+
+	@Test
+	@DisplayName("[SERVICE] Não deve atualizar um autor para o ID incorreto")
+	public void shouldNotUpdateAuthor() {
+		Long authorId = 2L;
+
+		when(authorRepository.findById(authorStub.getId())).thenReturn(Optional.of(authorStub));
+
+		NotFoundException exception = assertThrows(NotFoundException.class,
+				() -> authorService.update(updateAuthorDTO, authorId));
+
+		assertEquals("Não foi localizado nenhum autor com o id: #" + authorId, exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("[SERVICE] Deve retornar uma lista de autores")
+	public void shouldFindAllAuthors() {
+		List<Author> authorsStub = List.of(authorStub);
+
+		when(authorRepository.findAll()).thenReturn(authorsStub);
+
+		List<AuthorResponseDTO> authors = authorService.getAuthors();
+
+		assertNotNull(authors);
+		assertEquals(authorsStub.size(), authors.size());
+		assertEquals(authorsStub.get(0).getName(), authors.get(0).getName());
 	}
 	
 }
