5d927cf283e14bc476b3c46ab58de8c5472a49d1
Lucas Kissmann <lucaskissmann@gmail.com>
2024-08-22 10:52:03 -0300

    feat: add QueryProvider

---
+++ src/test/java/com/library/api/helpers/QueryProvider.java
@@ -0,0 +1,10 @@
+package com.library.api.helpers;
+
+public class QueryProvider {
+    public static final String resetDB = "/resetDB.sql";
+    public static final String insertAuthors = "/insertAuthors.sql";
+    public static final String insertBooks = "/insertBooks.sql";
+    public static final String insertRenters = "/insertRenters.sql";
+    public static final String insertRentals = "/insertRentals.sql";
+
+}
