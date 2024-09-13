5ebf088efb781200f2d6633f6b9f7cad0e46c1ce
Lucas Kissmann <lucaskissmann@gmail.com>
2024-09-13 10:33:04 -0300

    refactor: applicationContext to improve design patterns and flexibility

--- src/main/java/com/library/api/context/ApplicationContext.java
+++ src/main/java/com/library/api/context/ApplicationContext.java
@@ -3,9 +3,26 @@ package com.library.api.context;
 import java.time.LocalDate;
 
 public class ApplicationContext {
-    public static final String GOOGLE_API = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+    private static final ApplicationContext INSTANCE = new ApplicationContext();
+    private String googleApiUrl;
 
-    public static LocalDate today() {
+    private ApplicationContext() {
+        this.googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+    }
+
+    public static ApplicationContext getInstance() {
+        return INSTANCE;
+    }
+
+    public LocalDate today() {
         return LocalDate.now();
+    }
+
+    public String getGoogleApiUrl() {
+        return googleApiUrl;
+    }
+
+    public void setGoogleApiUrl(String googleApiUrl) {
+        this.googleApiUrl = googleApiUrl;
     }
 }
